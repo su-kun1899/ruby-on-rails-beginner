b65c578e50a76c07e4f5e8e8c3ead327296cea4e
su-kun1899 <higedrum.coz@gmail.com>
2017-03-21 00:23:28 +0900

    Add edit person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -14,9 +14,22 @@ class PeopleController < ApplicationController
     @person = Person.new
   end
 
+  def edit
+    @msg = 'edit data.[id = ' + params[:id] + ']'
+    @person = Person.find(params[:id])
+  end
+
   def create
     # noinspection RubyResolve
     Person.create(params.require(:person).permit(:name, :age, :mail))
+    # noinspection RubyResolve
+    redirect_to '/people'
+  end
+
+  def update
+    person = Person.find(params[:id])
+    # noinspection RubyResolve
+    person.update(params.require(:person).permit(:name, :age, :mail))
     # noinspection RubyResolve
     redirect_to '/people'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
   get 'people/input'
+  get 'people/edit/:id', to: 'people#edit'
   get 'people/:id', to: 'people#detail'
   post 'people/create'
+  patch 'people/edit/:id', to: 'people#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
