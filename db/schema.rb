b69ee5998901a9334b2a3c9933d98356dd7ed9a9
su-kun1899 <higedrum.coz@gmail.com>
2017-05-01 00:16:43 +0900

    Migrate message.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319141715) do
+ActiveRecord::Schema.define(version: 20170417143910) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "person_id"
+    t.text     "title"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.text     "name"
