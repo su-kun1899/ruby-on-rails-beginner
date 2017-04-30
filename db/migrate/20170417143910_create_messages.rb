6bd1c33454d41c82758b8be72e7412944f1ed351
su-kun1899 <higedrum.coz@gmail.com>
2017-05-01 00:16:38 +0900

    Fix typo.

--- db/migrate/20170417143910_create_messages.rb
+++ db/migrate/20170417143910_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.integer :person_id
-      t.test :title
+      t.text :title
       t.text :message
 
       t.timestamps
